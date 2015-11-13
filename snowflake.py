890ad0ab26159b162659d1cd63cebbeafbaa8c92
John Stechschulte <john.l.stechschulte@gmail.com>
2015-11-13 15:10:27 -0700

    now generating snowflake outlines

--- snowflake.py
+++ snowflake.py
@@ -2,38 +2,85 @@
 from tkinter import *
 from random import random
 
-N = 80
-iterations = 20
+N = 200
+iterations = 40
+
+#shape="star"
+shape="hexagon"
 
 scale = 4
 offset = 400
+
+r3 = 3**0.5
+
+
+#           05  15  25  35  45  55
+#         04  14  24  34  44  45
+#       03  13  23  33  34  35
+#     02  12  22  23  24  25
+#   01  11  12  13  14  15
+# 00  01  02  03  04  05
+
+def neighbors(i, j):
+  if i>j: return
+  if (i, j) == (0, 0):
+    for _ in range(6): yield (0, 1)
+    return
+  if (i, j) == (0, 1):
+    for r in ((0, 2), (1, 1), (0, 1), (0, 0), (0, 1), (1, 1)):
+      yield r
+    return
+  if i == 0:
+    for r in ((0, j+1), (1, j), (1, j-1), (0, j-1), (1, j-1), (1, j)):
+      yield r
+    return
+  if i == j:
+    for r in ((i, j+1), (i, j+1), (i-1, j+1), (i-1, j), (i-1, j), (i-1, j+1)):
+      yield r
+    return
+  if i == j-1:
+    for r in ((i, j+1), (i+1, j), (i, j), (i, j-1), (i-1, j), (i-1, j+1)):
+      yield r
+    return
+  for r in ((i, j+1), (i+1, j), (i+1, j-1), (i, j-1), (i-1, j), (i-1, j+1)):
+    yield r
+
+  if i+1 <= j:   yield (i+1, j)
+  if i+1 <= j-1: yield (i+1, j-1)
+  if i <= j-1:   yield (i, j-1)
+  if i-1 <= j:   yield (i-1, j)
+  if i-1 <= j+1: yield (i-1, j+1)
+  if i <= j+1:   yield (i, j+1)
+
 def t(x):
-    return scale*x + offset
+  return scale*x + offset
 
 def cube_to_hex(c):
-    return (c[0], c[1])
+  return (c[0], c[1])
 
 def hex_to_cube(h):
-    return (h[0], h[1], -h[1]-h[0])
+  return (h[0], h[1], -h[1]-h[0])
 
 def draw_point(x, y):
-    w.create_oval(t(x)+scale/2, t(-y)+scale/2, t(x)-scale/2, t(-y)-scale/2)
+  w.create_oval(t(x)+scale/3, t(-y)+scale/3, t(x)-scale/3, t(-y)-scale/3,
+      outline="white", fill="#aaaaff")
 
 def draw_cell(i, j):
-    if i>j: return
-    draw_point(j+i/2, i*3**0.5/2)
-    draw_point(i+j/2, j*3**0.5/2)
-    draw_point(j+i/2, -i*3**0.5/2)
-    draw_point(i+j/2, -j*3**0.5/2)
-    draw_point(-j-i/2, i*3**0.5/2)
-    draw_point(-i-j/2, j*3**0.5/2)
-    draw_point(-j-i/2, -i*3**0.5/2)
-    draw_point(-i-j/2, -j*3**0.5/2)
-    i, j = -i-j, i
-    draw_point(j+i/2, i*3**0.5/2)
-    draw_point(j+i/2, -i*3**0.5/2)
-    draw_point(-j-i/2, i*3**0.5/2)
-    draw_point(-j-i/2, -i*3**0.5/2)
+  if i>j: return
+  for (ii, jj) in ((i, j), (j, i), (-i-j, i)):
+    for xm in (-1, 1):
+      for ym in (-1, 1):
+        draw_point(xm*(jj+ii/2), ym*ii*r3/2)
+
+def draw_border(i1, j1, i2, j2):
+  for (ii1, jj1, ii2, jj2) in ((i1, j1, i2, j2), (j1, i1, j2, i2), (-i1-j1, i1, -i2-j2, i2)):
+    for xm in (-1, 1):
+      for ym in (-1, 1):
+        x1, y1 = xm*(jj1 + ii1/2), ym*ii1*r3/2
+        x2, y2 = xm*(jj2 + ii2/2), ym*ii2*r3/2
+        w.create_line(
+            t((x1+x2+(y2-y1)/r3)/2), t((y1+y2+(x1-x2)/r3)/2),
+            t((x1+x2-(y2-y1)/r3)/2), t((y1+y2-(x1-x2)/r3)/2))
 
 master = Tk()
 
@@ -44,61 +91,55 @@ cells = [[0]*N for _ in range(N)]
 next_cells = [[0]*N for _ in range(N)]
 cells[0][0] = 1
 
-
-#           05  15  25  35  45  55
-#         04  14  24  34  44  45
-#       03  13  23  33  34  35
-#     02  12  22  23 -24- 25
-#   01  11  12  13  14  15
-# 00  01  02  03  04  05
-
 def get_neighbors(i, j):
-    s = 0
-    if (i, j) == (0, 0):
-        return [0, 63][cells[0][1]]
-    if (i, j) == (0, 1):
-        return 32*cells[0][0] + 17*cells[0][1] + 10*cells[1][1] + 4*cells[0][2]
-    if i==0:
-        return 32*cells[0][j-1] + 17*cells[1][j-1] + 10*cells[1][j] + 4*cells[0][j+1]
-    if i==j:
-        return 33*cells[i-1][j] + 18*cells[i-1][j+1] + 12*cells[i][j+1]
-    if i==j-1:
-        return (32*cells[i][j-1] + 16*cells[i][j] + 8*cells[i+1][j] +
-                4*cells[i][j+1] + 2*cells[i-1][j+1] + cells[i-1][j])
-    return (32*cells[i][j-1] + 16*cells[i-1][j-1] + 8*cells[i-1][j] +
-            4*cells[i][j+1] + 2*cells[i-1][j+1] + cells[i-1][j])
+  s = 0
+  if (i, j) == (0, 0):
+    return [0, 63][cells[0][1]]
+  if (i, j) == (0, 1):
+    return 32*cells[0][0] + 17*cells[0][1] + 10*cells[1][1] + 4*cells[0][2]
+  if i==0:
+    return 32*cells[0][j-1] + 17*cells[1][j-1] + 10*cells[1][j] + 4*cells[0][j+1]
+  if i==j:
+    return 33*cells[i-1][j] + 18*cells[i-1][j+1] + 12*cells[i][j+1]
+  if i==j-1:
+    return (32*cells[i][j-1] + 16*cells[i][j] + 8*cells[i+1][j] +
+        4*cells[i][j+1] + 2*cells[i-1][j+1] + cells[i-1][j])
+  return (32*cells[i][j-1] + 16*cells[i-1][j-1] + 8*cells[i-1][j] +
+      4*cells[i][j+1] + 2*cells[i-1][j+1] + cells[i-1][j])
 
 # (current_state, sum_neighbor_states): prob
 rules = [[None]*64, [None]*64]
 for i in range(64):
-    if rules[0][i] is not None: continue
-    r = {0:0, 1:1, 3:0.2, 5:.1, 7:0, 9:0.2, 11:0.1, 13:0.1, 15:0, 21:.1,
-            23:0.1, 27:1, 31:1, 63:0}[i]
-    for _ in range(6):
-        rules[0][i] = r**2
-        i = (i>>1) + ((i&1)<<5)
-    r = {0:1, 1:1, 3:0.3, 5:0.5, 7:0.5, 9:1, 11:1, 13:1, 15:0.7, 21:0.5, 23:1,
-            27:0.8, 31:0.9, 63:1}[i]
+  if rules[0][i] is not None: continue
+  r = {0:0, 1:1, 3:0.2, 5:.1, 7:0, 9:0.2, 11:0.1, 13:0.1, 15:0, 21:.1,
+      23:0.1, 27:1, 31:1, 63:0}[i]
+  for _ in range(6):
+    rules[0][i] = r**2
+    i = (i>>1) + ((i&1)<<5)
+  r = {0:1, 1:1, 3:0.3, 5:0.5, 7:0.5, 9:1, 11:1, 13:1, 15:0.7, 21:0.5, 23:1,
+      27:0.8, 31:0.9, 63:1}[i]
 
-    for _ in range(6):
-        rules[1][i] = r**0.5
-        i = (i>>1) + ((i&1)<<5)
-
-rules[0][0] = 0
-rules[1][0] = 1
+  for _ in range(6):
+    rules[1][i] = r**0.5
+    i = (i>>1) + ((i&1)<<5)
 
 print(rules)
 
 for _ in range(iterations):
-    for i, r in enumerate(cells[:-1]):
-        for j, c in enumerate(r[:-1]):
-            next_cells[i][j] = 0
-            if random() < rules[cells[i][j]][get_neighbors(i,j)]:
-                next_cells[i][j] = 1
-    cells, next_cells = next_cells, cells
+  for i, r in enumerate(cells[:-1]):
+    for j, c in enumerate(r[:-1]):
+      next_cells[i][j] = 0
+      if shape=="hexagon" and i+j>=N/4: continue
+      if random() < rules[cells[i][j]][get_neighbors(i,j)]:
+        next_cells[i][j] = 1
+  cells, next_cells = next_cells, cells
 
 for i, r in enumerate(cells):
-    for j, c in enumerate(r):
-        if c: draw_cell(i, j)
+  for j, c in enumerate(r):
+    for ni, nj in neighbors(i, j):
+      if c:
+        if not cells[ni][nj]:
+          draw_border(i, j, ni, nj)
+
 
 mainloop()
