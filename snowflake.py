5b7c4f3465aa998f8b2d43222306055c9e6cbf2e
John Stechschulte <john.l.stechschulte@gmail.com>
2015-11-12 23:25:11 -0700

    hacky crap

--- snowflake.py
+++ snowflake.py
@@ -1,30 +1,40 @@
 #!/usr/bin/env python3
 from tkinter import *
 
-scale = 300
-offset = 150
+scale = 5
+offset = 300
 def t(x): return scale*x + offset
+
+drawn = set()
+def draw_point(x, y):
+    if (x, y) in drawn:
+        print("redrawn:", x, y)
+    drawn.add((x, y))
+    w.create_oval(t(x)+3, t(-y)+3, t(x)-3, t(-y)-3)
+
+def draw_cell(i, j):
+    if j<2*i-1: return
+    draw_point(j+(i%2)/2, 3**0.5/2*i)
+    if i>0: draw_point(j+(i%2)/2, -3**0.5/2*i)
+    if j>0:
+        draw_point(-j-(i%2)/2, 3**0.5/2*i)
+        if i>0: draw_point(-j-(i%2)/2, -3**0.5/2*i)
 
 master = Tk()
 
 w = Canvas(master, width=600, height=600)
 w.pack()
 
-snowflake = [(0.,0.), (1.,0.), (.5,3**.5/2)]
+cells = [[0]*10 for _ in range(5)]
+cells[0][0] = 1
+cells[0][1] = 1
+cells[0][2] = 1
+cells[1][1] = 1
+cells[1][2] = 1
+cells[1][3] = 1
 
-for _ in range(6):
-    for i in range(len(snowflake), 0, -1):
-        p1 = snowflake[i-1]
-        p2 = snowflake[i-2]
-        snowflake.insert(i-1, (p1[0]*2/3 + p2[0]*1/3, p1[1]*2/3 + p2[1]*1/3))
-        midpoint = (p1[0]*1/2 + p2[0]*1/2, p1[1]*1/2 + p2[1]*1/2)
-        perp = (p1[1] - p2[1], p2[0] - p1[0])
-        snowflake.insert(i-1, (midpoint[0] + perp[0] * 3**0.5/6,
-                midpoint[1] + perp[1] * 3**0.5/6))
-        snowflake.insert(i-1, (p1[0]*1/3 + p2[0]*2/3, p1[1]*1/3 + p2[1]*2/3))
-
-for i in range(len(snowflake)):
-    w.create_line(t(snowflake[i-1][0]), t(snowflake[i-1][1]),
-            t(snowflake[i][0]), t(snowflake[i][1]))
+for i, r in enumerate(cells):
+    for j, c in enumerate(r):
+        if c: draw_cell(i, j)
 
 mainloop()
